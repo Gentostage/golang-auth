4f6a24c71d174302cc1728dca80d8dbfc8ff1b8a
Татауров Леонид <leontat20@gmail.com>
2020-07-22 16:14:48 +0900

    CreateUser

--- go.mod
+++ go.mod
@@ -4,15 +4,14 @@ go 1.14
 
 require (
 	github.com/BurntSushi/toml v0.3.1
+	github.com/asaskevich/govalidator v0.0.0-20200428143746-21a406dcc535 // indirect
 	github.com/gin-gonic/gin v1.6.3
+	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible
 	github.com/go-playground/validator/v10 v10.3.0 // indirect
 	github.com/golang/protobuf v1.4.2 // indirect
-	github.com/json-iterator/go v1.1.10 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/sirupsen/logrus v1.6.0
 	go.mongodb.org/mongo-driver v1.3.5
-	golang.org/x/sys v0.0.0-20200720211630-cb9d2d5c5666 // indirect
+	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899
 	google.golang.org/protobuf v1.25.0 // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
--- internal/app/model/user.go
+++ internal/app/model/user.go
@@ -1,6 +1,11 @@
 package model
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/go-ozzo/ozzo-validation/is"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"golang.org/x/crypto/bcrypt"
+)
 
 type User struct {
 	ID        primitive.ObjectID `json:"id,omitempty"bson:"_id,omitempty"`
@@ -8,4 +13,21 @@ type User struct {
 	Password  string             `json:"password,omitempty" bson:"password,omitempty"`
 	LastName  string             `json:"last_name,omitempty" bson:"last_name,omitempty"`
 	FirstName string             `json:"first_name,omitempty" bson:"first_name,omitempty"`
+}
+
+func (u *User) Validate() error {
+	return validation.ValidateStruct(u,
+		validation.Field(&u.Email, validation.Required, is.Email),
+		validation.Field(&u.Password, validation.By(requiredIf(u.Password == "")), validation.Length(6, 100)),
+	)
+}
+
+func (u *User) GeneratePassword() error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	} else {
+		u.Password = string(hash)
+	}
+	return nil
 }
