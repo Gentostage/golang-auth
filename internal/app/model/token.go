6b2a359b07a2cd41a1f39c12d151ed329774b78e
1474net <gentostage@gmail.com>
2020-07-23 12:51:12 +0900

    model token

--- internal/app/model/token.go
+++ internal/app/model/token.go
@@ -1,9 +1,33 @@
 package model
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"golang.org/x/crypto/bcrypt"
+	"time"
+)
 
 type Token struct {
 	ID           primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-	RefreshToken byte               `bson:"refresh_token,omitempty"`
-	Alive        bool               `bson:"alive, omitempty"`
+	RefreshToken string             `bson:"refresh_token,omitempty"`
+	TimeToLive   time.Time          `bson:"time_to_live,omitempty"`
+	Alive        bool               `bson:"alive,omitempty"`
+}
+
+func (t *Token) GenerateHashToken(accsessToken string) error {
+	token := accsessToken + t.RefreshToken
+	hashToken, err := bcrypt.GenerateFromPassword([]byte(token), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	t.RefreshToken = string(hashToken)
+	return nil
+}
+
+func (t *Token) CompareTokens(refreshToken string, accsessToken string) error {
+	token := accsessToken + refreshToken
+	err := bcrypt.CompareHashAndPassword([]byte(t.RefreshToken), []byte(token))
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- internal/app/store/mongostore/tokenrepository.go
+++ internal/app/store/mongostore/tokenrepository.go
@@ -1,11 +1,31 @@
 package mongostore
 
+import (
+	"context"
+	"github.com/Gentostage/golang-auth/internal/app/model"
+)
+
 type TokenRepository struct {
 	store *Store
 }
 
-func (t *TokenRepository) Create() error {
+func (t *TokenRepository) Create(tokenRefresh *model.Token) error {
+	tokenStorage := t.store.db.Database("auth-go").Collection("tokens")
+	_, err := tokenStorage.InsertOne(context.TODO(), &tokenRefresh)
+	if err != nil {
+		return err
+	}
 	return nil
+}
+
+func (t *TokenRepository) Get(token *model.Token) (*model.Token, error) {
+	tokenBase := &model.Token{}
+	tokenStorage := t.store.db.Database("auth-go").Collection("tokens")
+	err := tokenStorage.FindOne(context.TODO(), token).Decode(tokenBase)
+	if err != nil {
+		return nil, err
+	}
+	return tokenBase, nil
 }
 
 func (t *TokenRepository) Close() error {
@@ -13,5 +33,9 @@ func (t *TokenRepository) Close() error {
 }
 
 func (t *TokenRepository) DeleteAll() error {
+	return nil
+}
+
+func (t *TokenRepository) DeleteOne() error {
 	return nil
 }
