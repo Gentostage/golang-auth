a43e76a81613c6bcbd0186d799de737bbb0fc3ee
1474net <gentostage@gmail.com>
2020-07-23 12:19:00 +0900

    jwt refresh and time life

--- internal/app/jwt/jwt_test.go
+++ internal/app/jwt/jwt_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"testing"
+	"time"
 )
 
 func TestAccessToken_Decode(t *testing.T) {
@@ -41,4 +42,41 @@ func TestAccessToken_Encode(t *testing.T) {
 	assert.NotEmpty(t, tokenData)
 	fmt.Println(tokenData)
 
+}
+
+func TestAccessToken_liveTime(t *testing.T) {
+	token := &AccessToken{
+		SecretKey:  "wqeqwesadsadasxdsadsadsadas",
+		TimeToLive: 10,
+	}
+	oldTime := time.Now().Add(-11 * time.Minute)
+	tokenStruct := &TokenStructData{
+		Header: struct {
+			Alg string `json:"alg"`
+			Typ string `json:"typ"`
+		}{},
+		Payload: struct {
+			User primitive.ObjectID `json:"user"`
+			Time time.Time          `json:"time"`
+		}{Time: oldTime},
+	}
+	err := token.liveTime(tokenStruct)
+	assert.Error(t, err)
+
+	newTime := time.Now().Add(time.Minute * 5)
+	tokenStruct.Payload.Time = newTime
+	err2 := token.liveTime(tokenStruct)
+	assert.NoError(t, err2)
+}
+
+func TestRefreshToken_Generate(t *testing.T) {
+	u := &model.User{
+		ID:       primitive.NewObjectID(),
+		Email:    "Example",
+		Password: "12312321",
+	}
+	token := RefreshToken{}
+	hash := token.Generate(u)
+	fmt.Println(hash)
+	assert.NotEmpty(t, hash)
 }
